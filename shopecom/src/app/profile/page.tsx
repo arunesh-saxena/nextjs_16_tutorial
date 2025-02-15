f58560471d3674c5180fe784264c63d50b5cf6d7
Arunesh Saxena <aruneshsaxena@gmail.com>
2025-02-15 16:19:17 +0530

    basic route implemented

---
+++ shopecom/src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <h1>Profile</h1>
+            <p>Welcome to the profile page. Here you can view and edit your profile information.</p>
+        </div>
+    );
+};
+
+export default ProfilePage;
