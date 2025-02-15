11810e6fb19062da399e1d54de42093777ea4679
Arunesh Saxena <aruneshsaxena@gmail.com>
2025-02-15 19:30:59 +0530

    title Metadata

--- shopecom/src/app/layout.tsx
+++ shopecom/src/app/layout.tsx
@@ -1,24 +1,28 @@
 export const metadata = {
-  title: 'Shopecom',
-  description: 'Shopecom | One point Store for all your needs',
-}
+  title: {
+    default: "Shopecom",
+    template: "%s  Shopecom",
+    absolute: "",
+  },
+  description: "Shopecom | One point Store for all your needs",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <header style={{ padding: '1rem', backgroundColor: '#f0f0f0' }}>
+        <header style={{ padding: "1rem", backgroundColor: "#f0f0f0" }}>
           <h1>Welcome to Shopecom page header</h1>
         </header>
         {children}
-        <footer style={{ padding: '1rem', backgroundColor: 'yellow' }}>
+        <footer style={{ padding: "1rem", backgroundColor: "yellow" }}>
           <p>&copy; 2023 Shopecom. All rights reserved. Footer</p>
         </footer>
       </body>
     </html>
-  )
+  );
 }
