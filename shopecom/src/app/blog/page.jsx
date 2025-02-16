4018b7d1607b4d81edb54b387bc9789de0a510c6
Arunesh Saxena <aruneshsaxena@gmail.com>
2025-02-16 10:37:22 +0530

    loading ui

--- shopecom/src/app/blog/page.jsx
+++ shopecom/src/app/blog/page.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const BlogPage = () => {
+const BlogPage = async () => {
+    await new Promise((resolve) => setTimeout(()=>{
+        resolve('Intentional delay');
+    }, 2000));
     return (
         <div>
             <h1>Blog Page</h1>
