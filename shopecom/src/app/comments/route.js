96ebcb33386b31dd591acae244d26e837ca80851
Arunesh Saxena <aruneshsaxena@gmail.com>
2025-02-25 10:41:10 +0530

    Handling POST Requests

--- shopecom/src/app/comments/route.js
+++ shopecom/src/app/comments/route.js
@@ -4,3 +4,17 @@ export async function GET(request) {
     return Response.json(comments);
 }
 
+export async function POST(request) {
+    const comment = await request.json();
+    const newComment  = {
+        id: comments.length + 1,
+        text: comment.text
+    }
+    comments.push(newComment);
+    return new Response(JSON.stringify(newComment), {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        status: 201
+    });
+}
