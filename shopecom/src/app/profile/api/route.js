859298708e8fe6d915b8c9a9f6dac59b6bb91a28
Arunesh Saxena <aruneshsaxena@gmail.com>
2025-03-05 09:59:37 +0530

    Cookies in Route Handlers

--- shopecom/src/app/profile/api/route.js
+++ shopecom/src/app/profile/api/route.js
@@ -1,10 +1,20 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
-export async function GET() {
+export async function GET(request) {
 
     const headersList = await headers();
+    const cookiesStore = await cookies();    
+    cookiesStore.set('resultPerPage', '20');
+
+    const theme = request.cookies.get('theme');
+
     console.log('Headers:', headersList.get('Authorization'));
+
+    console.log('Cookies:', theme, cookiesStore.get('resultPerPage'));
+
     return new Response('<h1>Profile Data</h1', {
-        headers: {  'Content-Type': 'text/html' }
+        headers: {  'Content-Type': 'text/html',
+            'Set-Cookie': 'theme=dark',
+         }
     });
 }
