a487cba616114a6e285bfa8a53e32f61a6f8d7e7
Arunesh Saxena <aruneshsaxena@gmail.com>
2025-03-16 16:36:11 +0530

    form validation and server action

--- shopecom/src/app/products-db-create/page.jsx
+++ shopecom/src/app/products-db-create/page.jsx
@@ -1,52 +1,64 @@
-import { addProduct } from "@/prisma-db";
-import { redirect } from "next/navigation";
-import {Submit} from "@/app/_components/submit";
+"use client";
 
+import { useActionState } from "react";
+import { createProduct } from "@/app/actions/products";
+import { Submit } from "@/app/_components/submit";
 
 export default function AddProductPage() {
+    const initialState = {
+        errors: {},
+    };
 
-    async function createProduct(formData) {
-        'use server';
-
-
-        const title = formData.get('title');
-        const price = formData.get('price');
-        const description = formData.get('description');
-
-        // console.log(title);
-        await addProduct(title, parseInt(price), description);
-
-        redirect('/products-db');
-    }
+    const [state, formAction] = useActionState(createProduct, initialState);
 
     return (
-        <form onSubmit={createProduct} className="p-4 space-y-4 max-w-96">
-            <label className="">
-                Title
-                <input
-                    type="text"
-                    className="block w-full p-2 text-black border rounded"
-                    name="title"
-                />
-            </label>
-            <label className="">
-                Price
-                <input
-                    type="number"
-                    className="block w-full p-2 text-black border rounded"
-                    name="price"
-                />
-            </label>
-            <label className="">
-                Description
-                <textarea
-                    className="block w-full p-2 text-black border rounded"
-                    name="description"
-                />
-            </label>
+        <form action={formAction} className="p-4 space-y-4 max-w-96">
+            <div>
+                <label className="">
+                    Title
+                    <input
+                        type="text"
+                        className="block w-full p-2 text-black border rounded"
+                        name="title"
+                    />
+                </label>
+                {state.errors.title && (
+                    <p className="text-red-500">{state.errors.title}</p>
+                )}
+            </div>
+            <div>
+                <label className="">
+                    Price
+                    <input
+                        type="number"
+                        className="block w-full p-2 text-black border rounded"
+                        name="price"
+                    />
+                </label>
+                {state.errors.price && (
+                    <p className="text-red-500">{state.errors.price}</p>
+                )}
+            </div>
+            <div>
+                <label className="">
+                    Description
+                    <textarea
+                        className="block w-full p-2 text-black border rounded"
+                        name="description"
+                    />
+                </label>
+                {state.errors.description && (
+                    <p className="text-red-500">{state.errors.description}</p>
+                )}
+            </div>
+            {/* <button
+        type="submit"
+        className="block w-full p-2  bg-blue-500 rounded disabled:bg-gray-500"
+        disabled={isPending}
+      >
+        Submit
+      </button> */}
             <Submit />
         </form>
     );
 }
-
-// export default AddProductPage;
