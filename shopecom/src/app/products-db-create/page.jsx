2d0e42f59daf4829fade96180ab01b34b0ea52c3
Arunesh Saxena <aruneshsaxena@gmail.com>
2025-03-16 16:09:51 +0530

    Pending State with useFormStatus

--- shopecom/src/app/products-db-create/page.jsx
+++ shopecom/src/app/products-db-create/page.jsx
@@ -1,5 +1,6 @@
 import { addProduct } from "@/prisma-db";
 import { redirect } from "next/navigation";
+import {Submit} from "@/app/_components/submit";
 
 
 export default function AddProductPage() {
@@ -43,12 +44,7 @@ export default function AddProductPage() {
                     name="description"
                 />
             </label>
-            <button
-                type="submit"
-                className="block w-full p-2  bg-blue-500 rounded disabled:bg-gray-500"
-            >
-                Add Product
-            </button>
+            <Submit />
         </form>
     );
 }
